40872318da7ed77633714367b0720be41e5426c4
Saurabh Nemade <saurabhnemade@users.noreply.github.com>
2018-12-09 20:28:43 +0530

    Using slf4j for logging

--- src/main/java/com/example/springbootannotationexample/aop/LogTimeAspect.java
+++ src/main/java/com/example/springbootannotationexample/aop/LogTimeAspect.java
@@ -3,17 +3,21 @@ package com.example.springbootannotationexample.aop;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LogTimeAspect {
+    Logger logger = LoggerFactory.getLogger(LogTimeAspect.class);
+
     @Around("@annotation(LogTime)")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
         Object proceed =  joinPoint.proceed();
         long executionTime = System.currentTimeMillis() - start;
-        System.out.println(joinPoint.getSignature() + " execution time :  " + executionTime + "ms");
+        logger.info(joinPoint.getSignature() + " execution time :  " + executionTime + "ms");
         return proceed;
     }
 }
